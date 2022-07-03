38e91f4e67dad5b143fb7a171d8f533e3c9a965c
PabloPalaciosAlonso <81316423+PabloPalaciosAlonso@users.noreply.github.com>
2022-07-03 22:22:04 +0200

    Add files via upload

--- fitAllParticles.py
+++ fitAllParticles.py
@@ -1,44 +1,60 @@
-import TVD
+import sys
+sys.path.insert(0, '../fitSquareWave')
+import squareWaveFit as SWF
 import scientificPlot as splt
 import utils as u
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+
 def askFolder():
-    folder = input("Where do you want to get the file?\n")
+    folder = input("Where do you want to get the file(s)?\n")
     return folder
 
-jumpWeight = 5000
+# Read input parameter
+inputs = u.readInputFile("data.main", inputs = ["jumpWeight", "minJump"],
+                       optionalInputs = ["plotFits", nPointsSmooth])
+
+jumpWeight = inputs["jumpWeight"]
+minJump = inputs["minJump"]
+plotFits = True
+nPoitnsSmooth = -1
+if "plotFits" in inputs.keys():
+    plotFits = bool(inputs["plotFits"])
+if "nPointsSmooth" in inputs.keys():
+    nPointsSmooth = inputs["nPointsSmooth"]
+    
+# Ask for a folder where search for the files
 folder = askFolder()
 files = os.listdir(folder)
+
+# Create a folder where save the fits and the fitting parameters
 os.makedirs(folder+"Fits", exist_ok=True)
 paramsFile = open(folder+"Fits/fittedParams.txt",'w')
 intensities = []
 onTimes = []
 for file in files:
+    # Change the conditional depending on the format of the input files
     if ".txt" in file and "particle_" in file:
         print(file)
-        params, intensity = TVD.fitParticle(folder, file, jumpWeight)
-        time, particle, background = TVD.readData(folder, file = file)
-        ax = None
-        ax = splt.plot(time/1000, particle, label = "Particle")
-        ax = splt.plot(time/1000, intensity, ax, label = "Fit")
-        ax.set_xlabel("Time (s)")
-        ax.set_ylabel("Intensity (A. U.)")
-        ax.legend()
-        nameFig = file.replace("txt","")
-        splt.saveFig(nameFig, folder = folder+"Fits")
-        plt.close()
+
+        # Fit the intensity of the file
+        params, intensity = SWF.fitParticle(folder, file, jumpWeight = jumpWeight, minJump = minJump)
+
+        #Write the parameters to a file
         paramsFile.write(" ".join(u.ntos(params))+"\n")
-        if len(params)>1:
-            intensities+=[params[1]-params[0]]
-            for i in range(1,round(len(params)/2)):
-                if params[i]==0:
-                    continue
-                if 2*i+1==len(params):
-                    continue
-                onTimes+=[params[2*i+1]]
+
+        #Plot the fit
+        if plotFits:
+            time, particle, background = SWF.readData(folder, file = file)
+            ax = None
+            ax = splt.plot(time/1000, SWF.smoothIntensity(particle,3), label = "Particle")
+            ax = splt.plot(time/1000, intensity, ax, label = "Fit")
+            ax.set_xlabel("Time (s)")
+            ax.set_ylabel("Intensity (A. U.)")
+            ax.legend()
+            nameFig = file.replace("txt","")
+            splt.saveFig(nameFig, folder = folder+"Fits")
+            plt.close()
 paramsFile.close()
-plt.figure(99)
-plt.hist(intensities, bins = 15)
-plt.show()
+
