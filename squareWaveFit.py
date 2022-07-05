d564b4ac24066a61370927d3e229f74f60ddda4e
PabloPalaciosAlonso <81316423+PabloPalaciosAlonso@users.noreply.github.com>
2022-07-05 11:45:24 +0200

    Add files via upload

--- squareWaveFit.py
+++ squareWaveFit.py
@@ -21,6 +21,7 @@ def readData(folder, file = None, particleId = None):
         if particleId is None:
             particleId = askParticle()
         filename = "particle_"+str(particleId)+".txt"
+    
     data = u.readMatrix(filename, folder = folder, remove = 3, delimiter = "   ")
     time = data[:,1]
     particle = data[:,2]
@@ -50,10 +51,12 @@ def squareWave(params, nFrames):
     """
     sw = np.ones(nFrames)*params[0]
     for i in range(1,int(len(params)/2)):
+        jumpStart = int(params[2*i])
         if (2*i+1)<(len(params)):
-            sw[round(params[2*i]):round(params[2*i]+params[2*i+1])] = params[1]
+            jumpEnd = jumpStart+int(params[2*i+1])
+            sw[jumpStart:jumpEnd] = params[1]
         else:
-            sw[round(params[2*i]):] = params[1]
+            sw[jumpStart:] = params[1]
     return sw 
 
 # Given a set of intensities, a baseline and an excitation energy returns a fit of the intensities such that
@@ -67,6 +70,7 @@ def squareWaveDistance(intensity, baseline, dI):
 
 # Computes error[i] = min((intensity-baseline)^2, (intensity-dI)^2)
 def errorFunctionFilteredIntensities(params):
+    global denoisedIntensity
     errorUpDown = np.zeros((2,len(particleIntensity)))
     errorUpDown[0,:] = (denoisedIntensity-params[0])**2
     errorUpDown[1,:] = (denoisedIntensity-params[1])**2
@@ -91,17 +95,8 @@ def getParamsOfSquareWave(intensities, baseline, dI):
         lastInt = newInt
     return params
 
-"""
-Principal function, reads the intensity of a particle and fit it in three steps:
-   First: Denoise the intensities using the TVD algorithm
-   Second: Fit the denoised intensity to a square wave 
-   Third: From the square wave obtained in step 2 uses the lm algorithm to fit the square wave
-          that best fits the intensity of the particle
-"""
-def fitParticle(folder, particleId=None, filename = None, jumpWeight = 5000, minJump = 2.5, npointsSmooth = 3):
-    global time, particleIntensity, background, denoisedIntensity
-    time, particleIntensity, background = readData(folder, particleId)
-    particleIntensity = smoothIntensity(particleIntensity, npointsSmooth)
+def applyAllFits(time, jumpWeight):
+    global particleIntensity, denoisedIntensity
     denoisedIntensity = TVD(particleIntensity, jumpWeight)
     fit1 = optimize.root(errorFunctionFilteredIntensities, [min(denoisedIntensity), max(denoisedIntensity)], method = 'lm')
     [baselineFit1, dIFit1] = fit1.x
@@ -109,17 +104,68 @@ def fitParticle(folder, particleId=None, filename = None, jumpWeight = 5000, min
     params = getParamsOfSquareWave(fitDenoisedIntensity, baselineFit1, dIFit1)
     fit2 = optimize.least_squares(dist_SquareWave_ParticleIntensities, params, method = "lm")
     fittedParams = fit2.x
+    return fittedParams
 
-    # We only consider excitations of dI>minJump*std(background)
-    if fit2.x[1]<np.std(background)*minJump:
+def removeSmallExcitations(params, background, minJump):
+    if params[1]-params[0]<np.std(background)*minJump:
         fittedParams = [np.mean(particleIntensity)]
+    else:
+        fittedParams = params
+    return fittedParams
 
+def removeShortExcitations(params, minDuration):
+    removedJumps = []
+    if minDuration>2 and len(params)>2:
+        njumps = int(len(params)*0.5)
+        goodJumps = []
+        for i in range(1, njumps):
+            if params[2*i+1]<=minDuration:
+                removedJumps+=[params[2*i], params[2*i+1]]
+    return removedJumps
+"""
+Principal function, reads the intensity of a particle and fit it in three steps:
+   First: Denoise the intensities using the TVD algorithm
+   Second: Fit the denoised intensity to a square wave 
+   Third: From the square wave obtained in step 2 uses the lm algorithm to fit the square wave
+          that best fits the intensity of the particle
+"""
+# Params:
+#   [0]          -> Baseline
+#   [1]          -> Excitation intensity
+#   [2*n (n>0)]  -> Start of a jump
+#   [2*n+1 (n>0)]-> Duration of the jump
+def fitParticle(folder, particleId=None, filename = None, jumpWeight = 2500, minJump = 2.5, npointsSmooth = 3, minDuration = 3):
+    global time, particleIntensity, background, denoisedIntensity
+    time, particleIntensity, background = readData(folder, particleId = particleId, file = filename)
+
+    # Smooth the data
+    particleIntensity = smoothIntensity(particleIntensity, npointsSmooth)
+    validFit = False
+    while not validFit: # Quite risky. TODO put some limit in the number of iterations
+        # Fit the data, with all the filters
+        fittedParams = applyAllFits(time, jumpWeight)
+    
+        # Remove the excitations that do not reach the threshold dI>minJump*std(background)
+        fittedParams = removeSmallExcitations(fittedParams, background, minJump)
+        #Remove the excitations that are not long enough
+        # We neglect excitation of duration nFrames<minDuration
+        removedJumps = removeShortExcitations(fittedParams, minDuration)
+        if len(removedJumps)>0: #One or more excitations have been removed
+            for i in range(int(0.5*len(removedJumps))):
+                excitationStart = int(removedJumps[2*i])
+                excitationEnd = excitationStart+int(removedJumps[2*i+1])
+                particleIntensity[excitationStart:excitationEnd] = fittedParams[0]
+        else:
+            validFit = True
+        #TODO: Remove excitations with most of the intensities under the baseline
+        
     fittedIntensity = squareWave(fittedParams, len(particleIntensity))
+
     # Transform the "on" times from frames to real units 
-    if len(fittedParams)>1:
+    if len(fittedParams)>2:
         for i in range(1, int(len(fittedParams)/2)):
-            startTime = time[int(fittedParams[2*i])]
-            endTime = time[int(fittedParams[2*i]+fittedParams[2*i+1])]
+            startTime = int(time[int(fittedParams[2*i])])
+            endTime = startTime+int(time[int(fittedParams[2*i+1])])
             fittedParams[2*i] = startTime
             fittedParams[2*i+1] = endTime-startTime
     return fittedParams, fittedIntensity
